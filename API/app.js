318d80f9aa7a83c14ec9e29e2b55609c80e4524b
Pau Torrents <pautorrents@MacBook-Pro-de-Pau.local>
2017-12-12 11:33:38 +0100

    Added cors

--- API/app.js
+++ API/app.js
@@ -7,6 +7,14 @@ var app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// CORS
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // ROUTES
 
 var postsRouter = require('./routes/postRoutes')
