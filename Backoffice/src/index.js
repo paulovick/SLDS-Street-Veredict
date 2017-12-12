91fd49d5b8b10e426e9b7404cbc1ebfb6bbde80c
Pau Torrents <pautorrents@MacBook-Pro-de-Pau.local>
2017-12-12 01:13:06 +0100

    React and redux test

--- Backoffice/src/index.js
+++ Backoffice/src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/external.min.css';
-import './css/site.css';
-import App from './js/App';
-import registerServiceWorker from './js/registerServiceWorker';
+import 'babel-polyfill'
+import registerServiceWorker from './js/registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//import './css/external.min.css'
+//import './css/site.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Root from './js/containers/Root'
+
+ReactDOM.render(
+    <Root />,
+    document.getElementById('root')
+)
+registerServiceWorker()
