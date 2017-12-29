ec05848692a4a3d2dfe5ce93d01c45e35ee909d5
EduardMaestro95 <eduardmaestro95@gmail.com>
2017-12-29 17:29:19 +0100

    Ficada llicencia a tots els arxius font

--- Backoffice/src/js/Authors/actions/authorContentActions.js
+++ Backoffice/src/js/Authors/actions/authorContentActions.js
@@ -1,3 +1,24 @@
+/*
+This file is part of Street Veredict.
+
+Street Veredict - An API/Website/Backoffice to share opinions on a given topic.
+Copyright (C) 2017  Pau Torrents I Gallego - Eduard Maestro Martinez
+
+Street Veredict is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Street Veredict is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Street Veredict program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 export const AUTHOR_PROPERTY_MODIFIED = 'AUTHOR_PROPERTY_MODIFIED'
 export const AUTHOR_JSON_VALIDATION_ERROR = 'AUTHOR_JSON_VALIDATION_ERROR'
 export const AUTHOR_COMPONENT_RESET = 'AUTHOR_COMPONENT_RESET'
--- Backoffice/src/js/Topics/actions/topicContentActions.js
+++ Backoffice/src/js/Topics/actions/topicContentActions.js
@@ -1,3 +1,24 @@
+/*
+This file is part of Street Veredict.
+
+Street Veredict - An API/Website/Backoffice to share opinions on a given topic.
+Copyright (C) 2017  Pau Torrents I Gallego - Eduard Maestro Martinez
+
+Street Veredict is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Street Veredict is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Street Veredict program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 export const TOPIC_PROPERTY_MODIFIED = 'TOPIC_PROPERTY_MODIFIED'
 export const TOPIC_JSON_VALIDATION_ERROR = 'TOPIC_JSON_VALIDATION_ERROR'
 export const TOPIC_COMPONENT_RESET = 'TOPIC_COMPONENT_RESET'
